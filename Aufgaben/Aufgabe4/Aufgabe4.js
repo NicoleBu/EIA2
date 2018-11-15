688a24960b2c85bcaaad8121c1bb1a9fccff5349
Nicole <Nicole@nicoles-air.fritz.box>
2018-11-15 18:46:46 +0100

    neu

---
+++ Aufgaben/Aufgabe4/Aufgabe4.js
@@ -0,0 +1,8 @@
+/*Aufgabe: Aufgabe 4 - Weihnachtsbaumkonfigurator
+Name: Nicole Burmistrack
+Matrikel: 259228
+Datum: 15.11.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/ 
+//# sourceMappingURL=Aufgabe4.js.map
---
+++ Aufgaben/Aufgabe4/Aufgabe4.ts
@@ -0,0 +1,20 @@
+/*Aufgabe: Aufgabe 4 - Weihnachtsbaumkonfigurator
+Name: Nicole Burmistrack
+Matrikel: 259228
+Datum: 15.11.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+
+namespace shopTree {
+    
+    interface xmasTree  {
+        
+        name: string;
+        price: string;
+        
+        }
+    
+    let trees: product [] = [{ name:"tree_0", price: 
+    
+    
