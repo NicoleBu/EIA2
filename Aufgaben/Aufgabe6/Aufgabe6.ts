9bcbea1c3281afb2d356a47eb1465efd6ae099d9
Nicole <Nicole@nicoles-air.eduroam.hs-furtwangen.de>
2018-11-29 20:10:40 +0100

    neu

--- Aufgaben/Aufgabe6/Aufgabe6.ts
+++ Aufgaben/Aufgabe6/Aufgabe6.ts
@@ -6,7 +6,7 @@ Datum: 21.11.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
-namespace xmasTreeShop3 {
+namespace xmasTreeShop2 {
 
     
     // EventListener
