372d8d0cce239b8700b096eaa63fe802260a6fdc
Nicole <Nicole@nicoles-air.eduroam.hs-furtwangen.de>
2018-11-29 18:50:24 +0100

    neu

--- Server/Server.js
+++ Server/Server.js
@@ -14,11 +14,11 @@ var L06_SendData;
         console.log("Listening"); //"Listening" wird durch console.log in der Browser Konsele ausgegeben.
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
-        "I hear voices"; //wird durch console.log auf der Browser Konsole ausgegeben
+        console.log("I hear voices!"); //"I hear voices" //wird durch console.log auf der Browser Konsole ausgegeben
         _response.setHeader("content-type", "text/html; charset=utf-8"); //Reaktion in Form von Information wird als Text, ("content-type", "text/html; charset=utf-8"), in festgelegtem Header ausgegeben.
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Reaktion in Form von Information wird als Text, ("Access-Control-Allow-Origin", "*"),  in festgelegtem Header ausgegeben.
         _response.write(_request.url); //_response greift auf die URL aus _request zu
+        console.log(_request.url);
         _response.end(); // _response wird beendet
     }
 })(L06_SendData || (L06_SendData = {}));
