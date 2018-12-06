9971df1201d8bd5a0eb346b64ea29e5612c0b4d9
Nicole <Nicole@nicoles-air.hfu-open.fu.hs-furtwangen.de>
2018-12-06 10:16:46 +0100

    neu

--- Server/Server.js
+++ Server/Server.js
@@ -1,25 +1,44 @@
 "use strict";
-const Http = require("http"); // importiert ein Objekt (Http), das Daten empfängt, von einem Modul ("http"), das importiert wird.
+const Http = require("http");
+const Url = require("url");
 var L06_SendData;
 (function (L06_SendData) {
-    console.log("Starting server"); // "Starting server" wird durch console.log in Browser Konsole ausgegeben.
-    let port = process.env.PORT; // port ist vom Typ number
+    console.log("Starting server");
+    let port = process.env.PORT;
     if (port == undefined)
-        port = 8100; // ... wird ihm der Wert 8100 zugesprochen.
-    let server = Http.createServer(); // verwandelt Computer in einer Http Server und erstellt und erstellt ein Http Server objekt.
-    server.addListener("request", handleRequest); // Listener wird auf Server erstellt ("request") und verweist auf die Funktion handleRequest
-    server.addListener("listening", handleListen); // Listener wird auf Server erstellt ("listening") und verweist auf die Funktion handleListen
-    server.listen(port); // Es wird ein Listener auf Port erstellt.
+        port = 8100;
+    let chosenProducts = [];
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
     function handleListen() {
-        console.log("Listening"); //"Listening" wird durch console.log in der Browser Konsele ausgegeben.
+        console.log("Listening");
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!"); //"I hear voices" //wird durch console.log auf der Browser Konsole ausgegeben
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Reaktion in Form von Information wird als Text, ("content-type", "text/html; charset=utf-8"), in festgelegtem Header ausgegeben.
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Reaktion in Form von Information wird als Text, ("Access-Control-Allow-Origin", "*"),  in festgelegtem Header ausgegeben.
-        _response.write(_request.url); //_response greift auf die URL aus _request zu
         console.log(_request.url);
-        _response.end(); // _response wird beendet
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        let url = _request.url;
+        if (url != "/favicon.ico") {
+            let section = Url.parse(url).search.substr(1);
+            let HTML = "";
+            for (let i = 0; i < section.length; i++) {
+                if (section[i] == "&") {
+                    chosenProducts.push(HTML);
+                    HTML = "<br>";
+                }
+                else {
+                    HTML += section[i];
+                }
+            }
+            chosenProducts.push(HTML);
+            for (let i = 0; i < chosenProducts.length; i++) {
+                _response.write(chosenProducts[i]);
+            }
+            console.log(chosenProducts);
+        }
+        _response.end();
     }
 })(L06_SendData || (L06_SendData = {}));
 //# sourceMappingURL=Server.js.map
--- Server/Server.ts
+++ Server/Server.ts
@@ -1,29 +1,56 @@
-import * as Http from "http";  // importiert ein Objekt (Http), das Daten empfängt, von einem Modul ("http"), das importiert wird.
+import * as Http from "http";
 
-namespace L06_SendData {  // über den namespace werden Objekte in einer Hierarchie bzw. Baumstruktur angeordnet und können über verschiedene Pfadnamen angesprochen werden.
-    console.log("Starting server");  // "Starting server" wird durch console.log in Browser Konsole ausgegeben.
-    let port: number = process.env.PORT; // port ist vom Typ number
-    if (port == undefined)   // wenn der port "undefined" ist, ...
-        port = 8100; // ... wird ihm der Wert 8100 zugesprochen.
+import * as Url from "url";
+namespace L06_SendData {
 
-    let server: Http.Server = Http.createServer(); // verwandelt Computer in einer Http Server und erstellt und erstellt ein Http Server objekt.
-    server.addListener("request", handleRequest);  // Listener wird auf Server erstellt ("request") und verweist auf die Funktion handleRequest
-    server.addListener("listening", handleListen); // Listener wird auf Server erstellt ("listening") und verweist auf die Funktion handleListen
-    server.listen(port); // Es wird ein Listener auf Port erstellt.
+    console.log("Starting server");
 
-    function handleListen(): void { //erstellt Funktion handle Listen vom Typ void
-        console.log("Listening"); //"Listening" wird durch console.log in der Browser Konsele ausgegeben.
+    let port: number = process.env.PORT;
+    if (port == undefined)
+        port = 8100;
+
+    let chosenProducts: string[] = [];
+    let server: Http.Server = Http.createServer();
+
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Listening");
+
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // Daten von _request werden von dem Http Element Incoming Message genommen. Daten von _response werden von dem Http Element ServerResponse genommen.
-        console.log("I hear voices!");//"I hear voices" //wird durch console.log auf der Browser Konsole ausgegeben
 
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Reaktion in Form von Information wird als Text, ("content-type", "text/html; charset=utf-8"), in festgelegtem Header ausgegeben.
-        _response.setHeader("Access-Control-Allow-Origin", "*"); //Reaktion in Form von Information wird als Text, ("Access-Control-Allow-Origin", "*"),  in festgelegtem Header ausgegeben.
-
-        _response.write(_request.url); //_response greift auf die URL aus _request zu
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log(_request.url);
-        _response.end(); // _response wird beendet
-        
+
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+
+
+        let url: string = _request.url;
+        if (url != "/favicon.ico") {
+            let section: string = Url.parse(url).search.substr(1);
+            let HTML: string = "";
+            for (let i: number = 0; i < section.length; i++) {
+                if (section[i] == "&") {
+                    chosenProducts.push(HTML);
+                    HTML = "<br>";
+                }
+                else {
+                    HTML += section[i];
+                }
+            }
+            chosenProducts.push(HTML);
+
+            for (let i: number = 0; i < chosenProducts.length; i++) {
+                _response.write(chosenProducts[i]);
+            }
+            console.log(chosenProducts);
+        }
+        _response.end();
     }
 }
